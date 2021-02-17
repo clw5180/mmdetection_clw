67be7d99b0ea086696656eedd74161eda1c97ed9
clw5180 <623497281@qq.com>
2021-02-17 23:37:21 +0800

    finish mosaic

--- mmdet/datasets/pipelines/loading.py
+++ mmdet/datasets/pipelines/loading.py
@@ -430,11 +430,95 @@ class LoadMosaicImageAndAnnotations(object):
             results = self._load_mosaic_image_and_annotations(results)
         return results
 
+    def _crop_patch(self, img, gt_bboxes, gt_labels, crop_size):
+
+        H, W, C = img.shape
+        px, py = crop_size  # 要切的patch的尺寸
+
+        if px > W or py > H:  # 如果要切的patch比图像本身还大, 那就只能pad原图了, 见下；
+            pad_w, pad_h = 0, 0
+
+            if px > W:
+                pad_w = px - W
+                W = px
+            if py > H:
+                pad_h = py - H
+                H = py
+
+            img = cv2.copyMakeBorder(img, 0, int(pad_h), 0, int(pad_w), cv2.BORDER_CONSTANT, 0)
+
+        obj_num = gt_bboxes.shape[0]
+        select_gt_id = np.random.randint(0, obj_num)  # 随机选择一个gt，准备在它四周切下一个patch
+        x1, y1, x2, y2 = gt_bboxes[select_gt_id, :]  # 选的某个gt，拿到它的 xyxy
+
+        if x2 - x1 > px:  # clw note: 如果bbox的宽度比要切的patch的宽度还宽
+            nx = np.random.randint(x1, x2 - px + 1)  # 就切一部分
+        else:  # clw note: 一般都是进这里
+            nx = np.random.randint(max(x2 - px, 0),
+                                   min(x1 + 1, W - px + 1))  # 个人感觉是不是 x1-1合适一些？甚至留一点边缘，那就是x1-border？ TODO
+
+        if y2 - y1 > py:
+            ny = np.random.randint(y1, y2 - py + 1)
+        else:
+            ny = np.random.randint(max(y2 - py, 0), min(y1 + 1, H - py + 1))
+
+        patch_coord = np.zeros((1, 4), dtype="int")
+        patch_coord[0, 0] = nx
+        patch_coord[0, 1] = ny
+        patch_coord[0, 2] = nx + px
+        patch_coord[0, 3] = ny + py
+
+        # index = self._compute_overlap(patch_coord, gt_bboxes, 0.5)
+        index = self._compute_overlap(patch_coord, gt_bboxes, 0.7)  # clw modify
+        index = np.squeeze(index, axis=0)
+        index[select_gt_id] = True  # 这里貌似写不写TRue都行，因为上面计算overlap，patch和内部的gt的overlap一定是1
+
+        patch = img[ny: ny + py, nx: nx + px, :]
+        gt_bboxes = gt_bboxes[index, :]
+        gt_labels = gt_labels[index]
+
+        gt_bboxes[:, 0] = np.maximum(gt_bboxes[:, 0] - patch_coord[0, 0], 0)  # 如果patch左边缘在gt左边缘的右边，那么这里就会算出负数，所以限制到0
+        gt_bboxes[:, 1] = np.maximum(gt_bboxes[:, 1] - patch_coord[0, 1], 0)
+        gt_bboxes[:, 2] = np.minimum(gt_bboxes[:, 2] - patch_coord[0, 0],
+                                     px - 1)  # 如果patch右边缘在gt右边缘左边，那么这里算出来的值就会比patch宽度还大，因此要限制到patch宽度
+        gt_bboxes[:, 3] = np.minimum(gt_bboxes[:, 3] - patch_coord[0, 1], py - 1)
+
+        return patch, gt_bboxes, gt_labels
+
+    def _compute_overlap(self, a, b, over_threshold=0.5):
+        """
+        Parameters
+        ----------
+        a: (N, 4) ndarray of float
+        b: (K, 4) ndarray of float
+        Returns
+        -------
+        overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+        """
+        area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+
+        iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
+        ih = np.minimum(np.expand_dims(a[:, 3], axis=1), b[:, 3]) - np.maximum(np.expand_dims(a[:, 1], 1), b[:, 1])
+
+        iw = np.maximum(iw, 0)
+        ih = np.maximum(ih, 0)
+
+        # ua = np.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
+        ua = area
+
+        ua = np.maximum(ua, np.finfo(float).eps)
+
+        intersection = iw * ih
+
+        overlap = intersection / ua
+        index = overlap > over_threshold
+        return index
+
     def _load_mosaic_image_and_annotations(self, results):
         indexes = [0, 1, 2, 3]
         result_boxes = []
         result_labels = []  # clw note: TODO !!!!!
-        results_c = copy.deepcopy(results)
+        results_c = copy.deepcopy(results)  # [ result, result, result, result ]  result: dict, 'img_info', 'ann_info', ....
         results = results[0]
         imsize = self.image_shape[0]
         #print(imsize)
@@ -446,24 +530,12 @@ class LoadMosaicImageAndAnnotations(object):
         for i, index in enumerate(indexes):
             result = self._load_image_annotations(results_c, index)
             #print(result.keys())
-            image = result['img'].astype(np.float32)
-            boxes = result['gt_bboxes']
+            img = result['img'].astype(np.float32)
+            ws, hs = img.shape[:2]  # clw note: small image
+
+            gt_bboxes = result['gt_bboxes']
+            gt_labels = result['gt_labels']
             # result_masks.append(result['gt_masks'])
-
-            ws, hs = image.shape[:2]  # clw note: small image
-
-            # if i == 0:
-            #     x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
-            #     x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
-            # elif i == 1:  # top right
-            #     x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
-            #     x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
-            # elif i == 2:  # bottom left
-            #     x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
-            #     x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
-            # elif i == 3:  # bottom right
-            #     x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
-            #     x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
 
             if i == 0:
                 x1a, y1a, x2a, y2a = max(xc - ws, 0), max(yc - hs, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
@@ -473,46 +545,58 @@ class LoadMosaicImageAndAnnotations(object):
                 x1a, y1a, x2a, y2a = max(xc - ws, 0), yc, xc, min(s * 2, yc + hs)
             elif i == 3:  # bottom right
                 x1a, y1a, x2a, y2a = xc, yc, min(xc + ws, s * 2), min(s * 2, yc + hs)
+            img_cropped, gt_bboxes_cropped, gt_labels_cropped = self._crop_patch(img, gt_bboxes, gt_labels, (x2a-x1a, y2a-y1a))
 
-            x1b = int(random.uniform(0, w - (x2a - x1a)))
-            y1b = int(random.uniform(0, h - (y2a - y1a)))
-            padw = x1a - x1b
-            padh = y1a - y1b
-            x2b = x2a - padw
-            y2b = y2a - padh
-
+            # x1b = int(random.uniform(0, w - (x2a - x1a)))
+            # y1b = int(random.uniform(0, h - (y2a - y1a)))
+            # padw = x1a - x1b
+            # padh = y1a - y1b
+            # x2b = x2a - padw
+            # y2b = y2a - padh
+            # result_image[y1a:y2a, x1a:x2a, :] = image[y1b:y2b, x1b:x2b, :]
             # print('result img:', result_image.shape, y1a,y2a, x1a,x2a, 'cut img:',  image.shape, y1b,y2b,  x1b,x2b) # for debug
-            result_image[y1a:y2a, x1a:x2a, :] = image[y1b:y2b, x1b:x2b, :]
 
-            # bbox coord recompute
-            boxes[:, 0] += padw
-            boxes[:, 1] += padh
-            boxes[:, 2] += padw
-            boxes[:, 3] += padh
+            result_image[y1a:y2a, x1a:x2a, :] = img_cropped
+            if i == 0:
+                pass
+            elif i == 1:  # top right
+                gt_bboxes_cropped[:, 0::2] += xc
+            elif i == 2:  # bottom left
+                gt_bboxes_cropped[:, 1::2] += yc
+            elif i == 3:  # bottom right
+                gt_bboxes_cropped[:, 0::2] += xc
+                gt_bboxes_cropped[:, 1::2] += yc
 
             # clean bbox out of region   TODO: this version is coarse
-            mask = (boxes[:, 2] > x1a) & (boxes[:, 0] < x2a) & (boxes[:, 3] > y1a) & (boxes[:, 1] < y2a)  # filter the gt which is out of the cropped region of origin image
-            boxes = boxes[mask]
+            # mask = (boxes[:, 2] > x1a) & (boxes[:, 0] < x2a) & (boxes[:, 3] > y1a) & (boxes[:, 1] < y2a)  # filter the gt which is out of the cropped region of origin image
+            # boxes = boxes[mask]
 
-            if boxes.size == 0:
-                boxes = np.zeros((0, 4), dtype=np.float32)
-                result_boxes.append(boxes)
+            if gt_bboxes_cropped.size == 0:
+                # boxes = np.zeros((0, 4), dtype=np.float32)
+                # result_boxes.append(boxes)
                 continue
             else:
-
-                # np.clip(labels[:, 1::2], x1b + padw, x2b + padw, out=labels[:, 1::2])
-                # np.clip(labels[:, 2::2], y1b + padh, y2b + padh, out=labels[:, 2::2])
-                np.clip(boxes[:, 0::2], x1a, x2a, out=boxes[:, 0::2])  # clw modify: the same with up
-                np.clip(boxes[:, 1::2], y1a, y2a, out=boxes[:, 1::2])
-                mask2 = (boxes[:, 2] - boxes[:, 0] > self.skip_box_w) & (boxes[:, 3] - boxes[:, 1] > self.skip_box_h) # filter the cropped gt which is too thin, which may lead to 'RuntimeError: CUDA error: device-side assert triggered' when build_target() if using hflip data_augment(index 13-0, 26-0, 52-0 cause this problem)
-                boxes = boxes[mask2]
-                result_boxes.append(boxes)
+                # np.clip(boxes[:, 0::2], x1a, x2a, out=boxes[:, 0::2])  # clw modify: the same with up
+                # np.clip(boxes[:, 1::2], y1a, y2a, out=boxes[:, 1::2])
+                # mask2 = (boxes[:, 2] - boxes[:, 0] > self.skip_box_w) & (boxes[:, 3] - boxes[:, 1] > self.skip_box_h) # filter the cropped gt which is too thin, which may lead to 'RuntimeError: CUDA error: device-side assert triggered' when build_target() if using hflip data_augment(index 13-0, 26-0, 52-0 cause this problem)
+                # boxes = boxes[mask2]
+                result_boxes.append(gt_bboxes_cropped)
+                result_labels.append(gt_labels_cropped)
 
         result_boxes = np.concatenate(result_boxes, 0)
+        result_labels = np.concatenate(result_labels, 0)
         # results = self._load_image_boxes(results, 0)
 
+        ### 可视化确认结果无误
+        # filename = results['img_info']['file_name']
+        # import cv2
+        # img_out = result_image.copy()
+        # for box in result_boxes:
+        #     cv2.rectangle(img_out, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), thickness=3, lineType=cv2.LINE_AA)
+        # cv2.imwrite('/home/user/' + filename, img_out )
+        ###
+
         results['ann_info']['bboxes'] = result_boxes
-        result_labels = np.zeros(len(result_boxes))
         results['ann_info']['labels'] = result_labels
 
         # masks = []
@@ -530,8 +614,7 @@ class LoadMosaicImageAndAnnotations(object):
         # results['ann_info']['masks'] = masks
 
         if results['img_prefix'] is not None:
-            filename = osp.join(results['img_prefix'],
-                                results['img_info']['filename'])
+            filename = osp.join(results['img_prefix'], results['img_info']['filename'])
         else:
             filename = results['img_info']['filename']
 
